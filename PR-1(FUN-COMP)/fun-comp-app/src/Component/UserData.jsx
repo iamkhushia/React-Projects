81c8ae8606054d4f202bda05b84a7ba507991013
iamkhushia <khushigondaliya736@gmail.com>
2025-01-22 23:16:06 +0530

    counter

--- PR-1(FUN-COMP)/fun-comp-app/src/Component/UserData.jsx
+++ PR-1(FUN-COMP)/fun-comp-app/src/Component/UserData.jsx
@@ -1,25 +1,25 @@
 
 import "./UserData.css";
 
-const UserDataCard = ({ name, email, phone, address, company, position, education, birthdate ,src}) => {
+const UserDataCard = ({ name, email, phone, address, company, Com_position, education, birthdate ,src}) => {
 
     return (
-        <div className="user-profile-card">
+        <div className="user-data-card">
             <h2>User Profile Card</h2>
             <img
                 src={src}
                 alt=""
             />
+            
             <h4>Name :- {name}</h4>
             <h4>Email :- {email}</h4>
             <h5>Phone :- {phone}</h5>
-            <details>
             <h5>Address :- {address}</h5>
             <h5>Birth Date :- {birthdate}</h5>
             <h5>Education :- {education}</h5>
             <h5>Company :- {company}</h5>
-            <h5>Position :- {position}</h5>
-            </details>
+            <h5>Com_position :- {Com_position}</h5>
+
         </div>
     );
 };
